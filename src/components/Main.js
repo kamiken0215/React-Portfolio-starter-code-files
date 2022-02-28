fe04f3edee65119a660ec1bd688fa99082e9713e
Kentaro Kamiyama <kamiken0215@gmail.com>
2022-03-01 06:36:12 +0900

    リンク装飾

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,6 +6,7 @@ import PowerButton from "../subComponents/PowerButton";
 import SocialIcons from "../subComponents/SocialIcons";
 import { YinYang } from "./AllSvgs";
 import Intro from "./Intro";
+import { motion } from "framer-motion";
 
 const MainContainer = styled.div`
   background: ${(props) => props.theme.body};
@@ -151,23 +152,33 @@ const Main = () => {
           target="_blank"
           to={{ pathname: "mailto:kamiken0215@gmail.com" }}
         >
-          <h2>Say hi...</h2>
+          <motion.h2 whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+            Say hi...
+          </motion.h2>
         </Contact>
 
         <BLOG to="/blog">
-          <h2>Blog</h2>
+          <motion.h2 whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+            Blog
+          </motion.h2>
         </BLOG>
 
         <WORK to="/work" click={click}>
-          <h2>Work</h2>
+          <motion.h2 whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+            Work
+          </motion.h2>
         </WORK>
 
         <BottomBar>
           <ABOUT to="/about" click={click}>
-            <h2>About.</h2>
+            <motion.h2 whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+              About.
+            </motion.h2>
           </ABOUT>
           <SKILLS to="/skills">
-            <h2>My Skills.</h2>
+            <motion.h2 whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+              My Skills.
+            </motion.h2>
           </SKILLS>
         </BottomBar>
       </Container>
