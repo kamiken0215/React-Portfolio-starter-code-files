1d32a36a4c1b2e134a4f611aedf811cc41b78b94
Kentaro Kamiyama <kamiken0215@gmail.com>
2022-03-15 22:50:38 +0900

    complete work page

--- src/components/WorkPage.js
+++ src/components/WorkPage.js
@@ -1,7 +1,76 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import styled, { ThemeProvider } from "styled-components";
+import { darkTheme } from "./Themes";
+import LogoComponent from "../subComponents/LogoComponent";
+import SocialIcons from "../subComponents/SocialIcons";
+import PowerButton from "../subComponents/PowerButton";
+
+import { Work } from "../data/WorkData";
+import Card from "../subComponents/Card";
+import { YinYang } from "./AllSvgs";
+
+const Box = styled.div`
+  background-color: ${(props) => props.theme.body};
+
+  height: 400vh;
+  position: relative;
+  overflow: hidden;
+`;
+
+const Main = styled.div`
+  position: fixed;
+  top: 12rem;
+  left: calc(10rem + 15vw);
+  height: 40vh;
+  display: flex;
+
+  color: white;
+`;
+
+const Rotate = styled.span`
+  display: block;
+  position: fixed;
+  right: 1rem;
+  bottom: 1rem;
+  width: 80px;
+  height: 80px;
+  z-index: 1;
+`;
 
 const WorkPage = () => {
-  return <div>WorkPage component page</div>;
+  const ref = useRef(null);
+  const yinYang = useRef(null);
+
+  useEffect(() => {
+    let element = ref.current;
+
+    const rotate = () => {
+      element.style.transform = `translateX(${-window.pageYOffset}px)`;
+      yinYang.current.style.transform = `rotate(` + window.pageYOffset + "deg)";
+    };
+
+    window.addEventListener("scroll", rotate);
+
+    return () => window.removeEventListener("scroll", rotate);
+  }, []);
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Box>
+        <LogoComponent theme="dark" />
+        <SocialIcons theme="dark" />
+        <PowerButton />
+        <Main ref={ref}>
+          {Work.map((d) => (
+            <Card key={d.id} data={d} />
+          ))}
+        </Main>
+        <Rotate ref={yinYang}>
+          <YinYang width={80} height={80} fill={darkTheme.text} />
+        </Rotate>
+      </Box>
+    </ThemeProvider>
+  );
 };
 
 export default WorkPage;
